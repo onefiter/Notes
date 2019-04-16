3fed10fd4b4aa96a8cd4268c002185896f7f2894
onefiter <onefiter@gmail.com>
2019-04-16 15:36:28 +0800

    Create syntax.php

---
+++ PHP/codes/php/syntax.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 1.下列语法不正确的是
+ */
+
+// echo HELLO;
+// print HELLO;
+// unset HELLO ; //不正确
+// include HELLO;
+
+/**
+ * 2.以下方法返回的是数组的是
+ *  A.file // 读整个文件到数组中
+ *  B.fopen // Returns a file pointer resource on success, or FALSE on error.
+ *  C.file_get_contents  //The function returns the read data or FALSE on failure.
+ *  D.readfile //Returns the number of bytes read from the file.
+ */
+
+
+ /**
+  * 3.Redhat Linux中，______不能直接反映系统负载的情况
+  *     A.sar B.top C.cat /proc/loadavg D.ps -aux
+  * 
+  * cat /proc/loadavg 最直接查看系统平均负载命令
+  * procps是一个实用程序包，主要包括ps top kill等程序主要用来显示与控制一些系统信息，进程状态之类的内容
+  * sar :System Activity Reporter系统活动情况报告
+  */
+
+
